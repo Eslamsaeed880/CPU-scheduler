9f40329ae23e46a811c936a674d4f0b9248a52cb
Fares A. Bakhit <faresa.bakhit@gmail.com>
2025-12-18 15:45:14 +0200

    Turn off context switching for AG

--- src/cpu/scheduler/AG.java
+++ src/cpu/scheduler/AG.java
@@ -17,6 +17,9 @@ public class AG implements IScheduler {
     }
 
     @Override
+    public boolean doContextSwitch() { return false; }
+
+    @Override
     public String /* Process name */ scheduleNext(int time) { // Runs for 1s.
         return "";
     }
